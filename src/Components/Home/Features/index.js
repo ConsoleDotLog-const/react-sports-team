5ae737c4d485d26ced5759c8669848a5764904ce
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-03-20 14:32:29 +1300

    font added, 1st part of main feature added

--- src/Components/Home/Features/index.js
+++ src/Components/Home/Features/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Stripes from './stripes'
+import Text from './text'
 
 const Features = () => {
     return (
         <div className="featured_wrapper">
             <Stripes/>
+            <Text/>
             
         </div>
     );
