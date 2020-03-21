bc33c94a6ff83a2ed50af7560ee1c6742b95afc5
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-03-21 23:26:38 +1300

    home pt 2 - matches created

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Features from './Features'
+import Matches from './Matches'
 
 const Home = () => {
     return (
         <div className="bck_blue">
             <Features/>
+            <Matches/>
         </div>
     );
 };
