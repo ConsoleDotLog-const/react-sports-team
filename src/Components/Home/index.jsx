749659408c1bcbc995d22b8fea6fd4aa4862031c
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-03-29 20:00:00 -0600

    MeetPlayers created

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Features from './Features'
 import Matches from './Matches'
+import MeetPlayers from './MeetPlayers'
 
 const Home = () => {
     return (
         <div className="bck_blue">
             <Features/>
             <Matches/>
+            <MeetPlayers/>
         </div>
     );
 };
