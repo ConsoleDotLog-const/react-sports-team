08a6e0f5ea14e4fd64ab62feebe589e30985b521
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-04-16 20:00:00 -0600

    dashboard refactored

---
+++ src/Components/Admin/Dashboard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import AdminLayout from '../../HOC/Admin_layout'
+
+
+
+const Dashboard = () => {
+    return (
+
+        <AdminLayout>
+            <div className="user_dashboard">
+             <div>
+                This is your Dashboard
+             </div>
+            
+            </div>
+
+
+        </AdminLayout>
+       
+    );
+};
+
+export default Dashboard 
--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,16 @@ import { Switch, Route} from 'react-router-dom'
 import Home from './Components/Home'
 import SignIn from './Components/signin'
 
+import Dashboard from './Components/Admin/Dashboard'
+
   
 const Routes =(props) => {
   return(
 
  <Layouts>
       <Switch>
+
+          <Route exact component={Dashboard} path ="/dashboard"/>
           <Route exact component={SignIn} path ="/sign_in"/>
           <Route exact component={Home} path ="/"/>
     
